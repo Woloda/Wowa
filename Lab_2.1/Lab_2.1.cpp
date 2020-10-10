20cc5f324179933fd1a238b03be564c0c3688998
Woloda <dorozovecvoloda@gmail.com>
2020-10-10 20:18:22 +0300

    Zmina 1

--- Lab_2.1/Lab_2.1.cpp
+++ Lab_2.1/Lab_2.1.cpp
@@ -7,17 +7,17 @@ int main()
 {
 	double x; // вхідний параметр 
 	double z1; // результат обчислення 1-го виразу 
-	double z2; // результат обчислення 2-го виразу 
+	// double z2; // результат обчислення 2-го виразу 
 
 	cout << "x = "; cin >> x;
 
 	z1 = cos(x) + cos(2 * x) + cos(6 * x) + cos(7 * x);
 
-	z2 = 4 * cos(x * 1.0 / 2.0) * cos(5 * x * 1.0 / 2.0) * cos(4 * x);
+	// z2 = 4 * cos(x * 1.0 / 2.0) * cos(5 * x * 1.0 / 2.0) * cos(4 * x);
 
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	// cout << "z2 = " << z2 << endl;
 
 	cin.get();
 	return 0;
